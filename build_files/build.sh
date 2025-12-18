f90bd3657082bc0f5b291c83e7fa987d3a625ffe
edgseu <amanb.singh21@gmail.com>
2025-12-18 15:54:01 +0530

    initial commit

---
+++ build_files/build.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+### Install packages
+
+# Packages can be installed from any enabled yum repo on the image.
+# RPMfusion repos are available by default in ublue main images
+# List of rpmfusion packages can be found here:
+# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+
+# this installs a package from fedora repos
+dnf5 -y copr enable peterwu/rendezvous
+
+dnf5 install -y @cosmic-desktop-environment
+dnf5 group install -y --with-optional virtualization
+
+# Base Packages
+PACKAGES=(
+    bibata-cursor-themes
+    papirus-icon-theme
+    gparted
+    java-25-openjdk
+)
+
+# Cosmic Packages
+PACKAGES+=(
+    NetworkManager-openvpn
+    xdg-user-dirs
+    gnome-keyring
+)
+
+# Remove Unneeded and Disable Repos
+# UNINSTALL_PACKAGES=(
+
+# )
+
+dnf5 install -y --allowerasing \
+    --setopt=install_weak_deps=False \
+    -x bluefin-readymade-config \
+    "${PACKAGES[@]}"
+
+
+# dnf5 remove -y "${UNINSTALL_PACKAGES[@]}"
+
+# Use a COPR Example:
+
+# dnf5 -y copr enable ublue-os/staging
+# dnf5 -y install package
+# Disable COPRs so they don't end up enabled on the final image:
+# dnf5 -y copr disable ublue-os/staging
+
+#### Example for enabling a System Unit File
+systemctl disable gdm 
+systemctl enable cosmic-greeter 
+systemctl enable libvirtd
+
+#systemctl enable podman.socket
+
+# Removing starship from system bashrc bashconfig, becuse I don't like bling in bash.
+sed -i.bak '/starship init bash/s/^/# /' /etc/bashrc
